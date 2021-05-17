1d2d1fb85c475e6c8143ac0fdd4b7f2f95005786
Fabian137 <55996513+Fabian137@users.noreply.github.com>
2021-05-17 10:53:25 -0500

    Actualizaciones | Catalogo,styles,js

---
+++ js/scriptV.js
@@ -0,0 +1,47 @@
+var productos =[
+  {
+    "producto": "Gran Reserva",
+    "precio": 5000,
+    "img": "img/calzado-BG.jpg"
+  },
+  {
+    "producto": "Chardonnay",
+    "precio": 9000,
+    "img": "img/calzado-Bla.jpg"
+  },
+  {
+    "producto": "Especial",
+    "precio": 900,
+    "img": "img/calzadoLgiht.jpg"
+  },
+  {
+    "producto": "Tradicional",
+    "precio": 1500,
+    "img": "img/calzado-running.jpg"
+  },
+  {
+    "producto": "Centenario Plata",
+    "precio": 3600,
+    "img": "img/calzado-runningGrey.jpg"
+  }/*,
+  {
+    "producto": "Nike Shoes 06",
+    "precio": 1000,
+    "img": "img/calzado-runningLight.jpg"
+  }*/
+]
+var cards = document.getElementById('cards')
+var total = 0;
+
+for(i=0; i<productos.length; i++){
+var products = productos[i]
+  cards.innerHTML += `
+  <div class="card" style="width: 12rem; margin-top: 1.5rem">
+    <img src="${products.img}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${products.producto}</h5>
+      <p class="card-text">$ ${products.precio}</p>
+      <a class="btn btn-primary" id="${i}">Añadir al carrito</a>
+    </div>
+  </div>`
+}
