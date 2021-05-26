e04fe8390a63a7eb8a429656213d0a8e5eb3443e
Fabian137 <55996513+Fabian137@users.noreply.github.com>
2021-05-25 23:42:21 -0500

    Funcionalidad catálogo 95%...

--- js/scriptV.js
+++ js/scriptV.js
@@ -101,11 +101,11 @@ var productos =[
   },
 ]
 var cards = document.getElementById('cards')
-
+  
 for(i=0; i<productos.length; i++){
 var products = productos[i]
   cards.innerHTML += `
-  <div class="card" style="width: 11rem; margin-top: 1.5rem">
+  <div class="card" style="width: 12rem; margin-top: 1.5rem">
     <img src="${products.img}" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${products.producto}</h5>
@@ -114,49 +114,65 @@ var products = productos[i]
     </div>
   </div>`
 }
+
 var addCar = document.querySelectorAll('.btn-primary');
-  addCar.forEach((addToCarB) =>{ /*-- Añade un onclick a cada botón de las cards --*/
-    addToCarB.addEventListener('click', forBuyClick)
-  })
-  
-  var infoCar = document.querySelector('.column');
-  function forBuyClick(event){ /*-- Agrega el producto al carrito --*/
-    var subtotals = []
-    const boton = event.target
-    const elemento = boton.closest('.card')
-    const idBoton = elemento.querySelector('.btn').id
-    infoCar.innerHTML +=`
-    <tr class="padreproduct">
-      <td>${productos[idBoton].producto}</td>
-      <td class="precioProducto" id="${idBoton}" type="number">${productos[idBoton].precio}</td>
-      <td><input class="input-quantity" type="number" value="1" onchange="quantityPr()"></td>
-      <td><button class="btn btn-danger" type="button" onclick="remove()">X</button></td>
-    </tr>`
-    updateShoppingCarTotal();
+addCar.forEach((addToCarB) =>{ /*-- Añade un onclick a cada botón de las cards --*/
+  addToCarB.addEventListener('click', forBuyClick)
+})
+
+var infoCar = document.querySelector('.column');
+function forBuyClick(event){
+  const boton = event.target
+  const item = boton.closest('.card')
+  var proname = item.querySelector('.card-title').textContent
+  var protitle = infoCar.getElementsByClassName('protitle')
+  for (let i = 0; i < protitle.length; i++) {
+    if (protitle[i].innerText === proname) {
+      let elementQuantity = protitle[i].parentElement.querySelector('.input-quantity')
+      elementQuantity.value++
+      updateTotalVal()
+      return;
+    }
+  }
+  // const boton = event.target
+  const elemento = boton.closest('.card')
+  const idBoton = elemento.querySelector('.btn').id
+  infoCar.innerHTML +=`
+  <tr class="padreproduct">
+    <td class="protitle">${productos[idBoton].producto}</td>
+    <td class="precioProducto" id="${idBoton}" type="number">${productos[idBoton].precio}</td>
+    <td><input class="input-quantity" type="number" value="1" onchange="quantityPr()"></td>
+    <td><button class="btn btn-danger" type="button" onclick="remove()">X</button></td>
+  </tr>`
+  updateTotalVal();
 }
 
-const quantityPr = () =>{
-  var cant = event.target
-  var elemento = cant.closest('.padreproduct')
-  var input = elemento.querySelector('.input-quantity')
-  input.value <= 0 ? (input.value = 1): null
-  updateShoppingCarTotal()
+const quantityPr = () =>{ /*-- Debería agregar o actualizar la el TOTAL cuando la cantidad de un producto del carrito cambia. Debería --*/
+var cant = event.target
+var elemento = cant.closest('.padreproduct')
+var input = elemento.querySelector('.input-quantity')
+input.value <= 0 ? (input.value = 1): null
+updateTotalVal()
 }
 /*--------UPADTE TOTAL--------*/
-const updateShoppingCarTotal = () =>{ 
-  var total = 0
-  let showTotal = document.getElementById('total')
-  var cartItems = document.querySelectorAll('.padreproduct');
-  cartItems.forEach((cartItem) => {
-    var cartItemPrice = parseFloat(cartItem.querySelector('.precioProducto').textContent)
-    var cartItemQuantityE = parseFloat(cartItem.querySelector('.input-quantity').value);
-    total += cartItemPrice * cartItemQuantityE;
-  });
-  showTotal.innerHTML = `${total}`;
-  }
-  
-const remove = () =>{ /* --Elimina un producto del carrito cuando le damos al tache --*/
+const updateTotalVal = () =>{ 
+var total = 0
+let showTotal = document.getElementById('total')
+var cartItems = document.querySelectorAll('.padreproduct');
+cartItems.forEach((cartItem) => {
+  var cartItemPrice = parseFloat(cartItem.querySelector('.precioProducto').textContent)
+  var cartItemQuantityE = parseFloat(cartItem.querySelector('.input-quantity').value);
+  total += cartItemPrice * cartItemQuantityE;
+});
+showTotal.innerHTML = `${total}`;
+}
+
+const remove = () =>{
   const buttonClicked = event.target
   buttonClicked.closest('tr').remove()
-  updateShoppingCarTotal()
+  updateTotalVal()
+  }
+const comprar = () =>{
+  infoCar.innerHTML = `Gracias por su compra!`;
+  updateTotalVal()
 }
