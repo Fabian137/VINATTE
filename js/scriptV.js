bfa6c6b6bee10d53df95941e1c688402ec2c554e
Fabian137 <55996513+Fabian137@users.noreply.github.com>
2021-05-25 21:44:21 -0500

    Funcionalidad catálogo 90% | Rediseño un poquito

--- js/scriptV.js
+++ js/scriptV.js
@@ -27,7 +27,7 @@ var productos =[
   {
     "producto": "Paloma",
     "precio": 5980,
-    "img": "img/botella/Centenario Plata.png"
+    "img": "img/botella/Paloma.png"
   },
   {
     "producto": "Chenin Blanc",
@@ -101,12 +101,11 @@ var productos =[
   },
 ]
 var cards = document.getElementById('cards')
-var total = 0;
 
 for(i=0; i<productos.length; i++){
 var products = productos[i]
   cards.innerHTML += `
-  <div class="card" style="width: 12rem; margin-top: 1.5rem">
+  <div class="card" style="width: 11rem; margin-top: 1.5rem">
     <img src="${products.img}" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${products.producto}</h5>
@@ -114,4 +113,50 @@ var products = productos[i]
       <a class="btn btn-primary" id="${i}">Añadir al carrito</a>
     </div>
   </div>`
+}
+var addCar = document.querySelectorAll('.btn-primary');
+  addCar.forEach((addToCarB) =>{ /*-- Añade un onclick a cada botón de las cards --*/
+    addToCarB.addEventListener('click', forBuyClick)
+  })
+  
+  var infoCar = document.querySelector('.column');
+  function forBuyClick(event){ /*-- Agrega el producto al carrito --*/
+    var subtotals = []
+    const boton = event.target
+    const elemento = boton.closest('.card')
+    const idBoton = elemento.querySelector('.btn').id
+    infoCar.innerHTML +=`
+    <tr class="padreproduct">
+      <td>${productos[idBoton].producto}</td>
+      <td class="precioProducto" id="${idBoton}" type="number">${productos[idBoton].precio}</td>
+      <td><input class="input-quantity" type="number" value="1" onchange="quantityPr()"></td>
+      <td><button class="btn btn-danger" type="button" onclick="remove()">X</button></td>
+    </tr>`
+    updateShoppingCarTotal();
+}
+
+const quantityPr = () =>{
+  var cant = event.target
+  var elemento = cant.closest('.padreproduct')
+  var input = elemento.querySelector('.input-quantity')
+  input.value <= 0 ? (input.value = 1): null
+  updateShoppingCarTotal()
+}
+/*--------UPADTE TOTAL--------*/
+const updateShoppingCarTotal = () =>{ 
+  var total = 0
+  let showTotal = document.getElementById('total')
+  var cartItems = document.querySelectorAll('.padreproduct');
+  cartItems.forEach((cartItem) => {
+    var cartItemPrice = parseFloat(cartItem.querySelector('.precioProducto').textContent)
+    var cartItemQuantityE = parseFloat(cartItem.querySelector('.input-quantity').value);
+    total += cartItemPrice * cartItemQuantityE;
+  });
+  showTotal.innerHTML = `${total}`;
+  }
+  
+const remove = () =>{ /* --Elimina un producto del carrito cuando le damos al tache --*/
+  const buttonClicked = event.target
+  buttonClicked.closest('tr').remove()
+  updateShoppingCarTotal()
 }
